b4f3706b896c535b290d249e3baeb3f37e574051
ishadow297 <baronalexis@icloud.com>
2026-04-09 23:12:53 +0200

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from supabase import create_client
-from datetime import datetime, timedelta
+from datetime import datetime
 import pytz, time
 
 # --- CONNEXION ---
@@ -31,13 +31,26 @@ def piloter(data):
                 nom = r.split(" [")[0]
                 t = r.split("[")[1].replace("]","")
                 d_s, f_s = t.split(" - ")
-                dt_d = datetime.strptime(f"{d_s}/{now.year}","%d/%m %H:%M/%Y").replace(tzinfo=tz)
-                dt_f = datetime.strptime(f"{f_s}/{now.year}","%d/%m %H:%M/%Y").replace(tzinfo=tz)
-                # Si le créneau est en cours
+                dt_d = datetime.strptime(f"{d_s}/{now.year} %H:%M","%d/%m/%Y %H:%M").replace(tzinfo=tz)
+                dt_f = datetime.strptime(f"{f_s}/{now.year} %H:%M","%d/%m/%Y %H:%M").replace(tzinfo=tz)
                 if dt_d <= now <= dt_f:
                     user, h_f = nom, f_s
                     new_f.append(r)
                 elif dt_d > now: new_f.append(r)
             except: new_f.append(r)
         
-        if b['statut']
+        if b['statut'] != "panne":
+            supabase.table("bornes").update({
+                "statut":"occupé" if user else "libre",
+                "utilisateur":user if user else "",
+                "fin":h_f if h_f else "",
+                "suivant":" | ".join(new_f)
+            }).eq("id",bid).execute()
+
+st.title("⚡ Bornes Calais")
+st.write(f"🕒 **{now.strftime('%d/%m %H:%M')}**")
+
+try:
+    data = supabase.table("bornes").select("*").order("id").execute().data
+    piloter(data)
+    data = supabase.table("bornes").select("*").
