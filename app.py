24d46cbe093c9bdb91ff76393b0853814294bb00
ishadow297 <baronalexis@icloud.com>
2026-04-09 10:59:18 +0200

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pytz
 
 # --- CONFIGURATION ---
 SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyv62yWQatjNv1mGN64afctC8IU7p94r6aNhzCTnTlKlvqQ-o2ob_5c3zwLEiaBX9TI/exec"
-SHEET_CSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQpyeQVt9fmmpJUEft_YjO52_ivj7gvJxcTTK53R0P3ptPIuKE2-v7pF9TwTJ5PPANlmzMkQwjIinow/pubhtml"
+SHEET_CSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQpyeQVt9fmmpJUEft_YjO52_ivj7gvJxcTTK53R0P3ptPIuKE2-v7pF9TwTJ5PPANlmzMkQwjIinow/pub?output=csv"
 
 st.set_page_config(page_title="Bornes Calais Pro", page_icon="⚡", layout="centered")
 
